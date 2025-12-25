767551698a4da72b6b387e5c210f44758f780618
Lucasgrn <lucasgabriel.ogn@gmail.com>
2025-12-25 12:22:51 -0300

    feat: configure nativewind

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-row">
+      <Text className="text-red-900">Edit app/index.tsx to edit this screen.</Text>
+      <Text>Alguma coisa escrita ao lado</Text>
+    </View>
+  );
+}
